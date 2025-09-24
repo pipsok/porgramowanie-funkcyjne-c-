ba9edf2bdd50fd1cb6d24b0d86b6e77cb0d7a290
pipsok <132793773+pipsok@users.noreply.github.com>
2025-09-24 09:21:28 +0200

    Add files via upload

---
+++ repos/test1/test1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Random rnd = new Random();
+            int liczba = rnd.Next(1,101);
+            int input = int.Parse(Console.ReadLine());
+            int proby = 0;
+            while (input != liczba) {
+                proby++;
+                input = int.Parse(Console.ReadLine());
+                if (input != liczba)
+                {
+                    Console.WriteLine("niestety nie zgadles sporobj jeszcze raz, proby: " + proby);
+                }
+            }
+            if (input == liczba) {
+                Console.WriteLine("brawo zgadles, prob:" + proby);
+                if (proby == 1) {
+                    Console.WriteLine("wow zgadles za pierwszym razem brawo !!!!!!!!!!!!!");
+                }
+            }
+            Console.WriteLine();
+            
+        }
+    }
+}
---
+++ repos/test2/test2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("siemanko ile pijesz kaw codziennie");
+            int kawycodz = int.Parse(Console.ReadLine());
+            Console.WriteLine("ile masz lat?");
+            int lata = int.Parse(Console.ReadLine());
+            Console.WriteLine("ile dziennie programujesz");
+            int godzinyprogramowania = int.Parse(Console.ReadLine());
+
+            int dnidoemerytury = (65 - lata) * 365;
+            int kawywroku = 365 * kawycodz;
+            int kawydoemerytury = kawycodz * dnidoemerytury;
+            int godzinykodowaniawroku = godzinyprogramowania * 365;
+
+            double procentzyciaprogramowania = godzinyprogramowania / 24 * 100;
+
+
+        }
+    }
+}
